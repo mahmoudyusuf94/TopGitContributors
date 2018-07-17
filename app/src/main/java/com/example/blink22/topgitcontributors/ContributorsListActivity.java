b2da017664cb8e925529dbb733d3c9ff24dbb9dc
Mahmoud Yusuf <mahmoud.yusuf@blink22.com>
2018-07-17 16:30:39 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/blink22/topgitcontributors/ContributorsListActivity.java
@@ -0,0 +1,16 @@
+package com.example.blink22.topgitcontributors;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContributorsListActivity extends SingleFragmentActivity {
+
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+
+}
