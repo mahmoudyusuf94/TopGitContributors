b77fa7863303f33d58ac2e47cb0bb7296da8bd2a
Mahmoud Yusuf <mahmoud.yusuf@blink22.com>
2018-07-17 17:14:52 +0200

    Added the retrofit instance and GET interface

---
+++ app/src/main/java/com/example/blink22/topgitcontributors/GetDataService.java
@@ -0,0 +1,13 @@
+package com.example.blink22.topgitcontributors;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface GetDataService {
+
+    @GET("/stats/contributors")
+    Call<List<Contributor>> getAllContributors();
+
+}
